8900757857801f8ec52165e6f06e20309de6d453
stukene <estuken53@gmail.com>
2020-09-10 10:28:20 -0700

    Outline Property Card

---
+++ src/components/PropertyCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.scss';
+
+const PropertyCard= () => {
+
+
+
+
+    return (
+        <div>
+            <NavBar /> 
+
+        </div>
+    );
+
+};
+
+export default PropertyCard;
