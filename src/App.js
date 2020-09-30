43151246b9d1abbfdd27ea0cee8f0ce3f2ef7917
stukene <estuken53@gmail.com>
2020-09-30 15:02:50 -0700

    fixed browser issue

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Homepage from './containers/Homepage.js';
 import Properties from './containers/Properties.js';
-import NavBar from '../components/NavBar.js'
-import { Router, Route, Switch } from "react-router";
+import NavBar from './components/NavBar.js'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useHistory } from 'react-router-dom';
 
-const App = () => {
+class App extends Component {
   state = {
     selected: 'home'
   }
 
-  return (
-    <div >
-      <NavBar />
-      <Router>
-        <Switch>
-          <Route exact path='/' >
-            <Homepage></Homepage>
-          </Route>
-          <Route exact path='/properties' >
-            <Properties></Properties>
-          </Route>
-        </Switch>
-      </Router>
-       
-    </div>
-  );
+  componentDidMount() {
+
+
+    
+  }
+
+  render () {
+    return (
+      <div>
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' >
+              <Homepage />
+            </Route>
+            <Route exact path='/properties' >
+              <Properties />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+
 }
 
 export default App;
