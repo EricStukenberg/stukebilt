eceafceee72780d4469e5c81691b860e8124bc60
stukene <estuken53@gmail.com>
2020-10-09 17:06:31 -0700

    added base

---
+++ src/base.js
@@ -0,0 +1,24 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDV5pIFVf1xZAuRM1_BCq2iADh1lXDhUrI",
+    authDomain: "stukebilt.firebaseapp.com",
+    databaseURL: "https://stukebilt.firebaseio.com",
+    projectId: "stukebilt",
+    storageBucket: "stukebilt.appspot.com",
+    messagingSenderId: "108267098942",
+    appId: "1:108267098942:web:426731edb416bc2bb7126f",
+    measurementId: "G-YG6L711G3C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
